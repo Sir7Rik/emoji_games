55575cfa75d9cfba3d874a4742848e377c59df07
Sir7Rik <76644024+Sir7Rik@users.noreply.github.com>
2020-12-27 13:42:18 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+(function init(){
+    const cards = Array.from(document.querySelectorAll('.card'));
+    cards.forEach((card) => {card.addEventListener('clik', clickEvent); });
+});
+
+function clickEvent(){
+    const opened = this.classList.gonstains('open');
+    if (!opened){
+        this.classList.remove('close');
+        this.classList.add('open');
+    } else {
+        this.classList.remove('close');
+        this.classList.add('open')
+    }
+}
