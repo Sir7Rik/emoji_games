017e30204646e4e26a8834b8566d61a31c8cc6f8
Sir7Rik <76644024+Sir7Rik@users.noreply.github.com>
2021-01-17 11:34:32 +0300

    new

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-(function init(){
-    const cards = Array.from(document.querySelectorAll('.card'));
-    cards.forEach((card) => {card.addEventListener('clik', clickEvent); });
-});
 
-function clickEvent(){
-    const opened = this.classList.gonstains('open');
-    if (!opened){
-        this.classList.remove('close');
-        this.classList.add('open');
+function shuffleEmoji(arr){
+    return arr.sort(() => Math.random()-0.5);
+}
+
+function coupleArr(arr){
+    return[].contact(arr, arr);
+}
+
+function fillCards(cards, emoji){
+    cards.forEach((card, index)=>{card.textContent = emoji[index]});
+}
+
+(function init() {
+    const emoji = '🎸 🥁 🎻 🎷 🎤 🎹'
+    const cards = document.querySelectorAll('.card');
+    fillCards(cards, shuffleEmoji);
+    cards.forEach((card) => { card.addEventListener('click', clickEvent); });
+}());
+
+function clickEvent() {
+    const opened = this.classList.contains('open');
+    if (!opened) {
+      this.classList.remove('close');
+      this.classList.add('open');
     } else {
-        this.classList.remove('open');
-        this.classList.add('close')
+      this.classList.remove('open');
+      this.classList.add('close');
     }
 }
